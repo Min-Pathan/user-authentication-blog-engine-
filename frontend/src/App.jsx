669ed20df5a2a945eda143c116bf561f3d7c8c80
Minaz Pathan <minpathan17@gmail.com>
2026-08-15 00:35:55 +0530

    chore: restructure project into bcakend and frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <main>
+      <h1>Blogger Frontend</h1>
+
+      <p>
+        The React frontend is running successfully.
+      </p>
+    </main>
+  );
+}
+
+export default App;
